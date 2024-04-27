fc9505cee8271b678b210afb6a6b5e9b9de5b1ac
Rabjho <rabjho@protonmail.com>
2024-04-27 12:12:09 +0200

    Fixed next button

--- src/components/Survey/PrevNextButtons.tsx
+++ src/components/Survey/PrevNextButtons.tsx
@@ -1,5 +1,5 @@
-// import { useContext } from "react";
-// import { SurveyContext } from "./Survey";
+import { useContext } from "react";
+import { SurveyContext } from "./Survey";
 
 interface PrevNextButtonsProps {
   onPrev: () => void;
@@ -14,14 +14,17 @@ export default function PrevNextButtons({
   currentStep,
   totalSteps,
 }: PrevNextButtonsProps) {
-  // const answers = useContext(SurveyContext);
+  const answers = useContext(SurveyContext);
+  const numberOfQuestions = answers ? Object.keys(answers).length : 0;
 
   return (
     <div className="horizontal center-h w-full *:mx-1 *:my-1 *:bg-slate-200 *:p-1">
       {currentStep != 0 && <button onClick={onPrev}>Previous</button>}
-      {currentStep != totalSteps - 1 && currentStep != 0 && (
-        <button onClick={onNext}>Next</button>
-      )}
+      {currentStep != totalSteps - 1 &&
+        currentStep != 0 &&
+        currentStep < numberOfQuestions && (
+          <button onClick={onNext}>Next</button>
+        )}
     </div>
   );
 }
